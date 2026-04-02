9d2f6936beee9783ab7c2a71a4c7446a931d6957
Thomas Riedel <thomas.riedel@worldiety.de>
2026-04-02 11:54:31 +0200

    chore: improved footer with click-to-copy versions text

--- cmd/nago-demo-srv/main.go
+++ cmd/nago-demo-srv/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	_ "embed"
+	"fmt"
+	"time"
 
 	"github.com/worldiety/nago-demo/pages/content"
 	"github.com/worldiety/nago-demo/pages/home"
@@ -9,7 +11,6 @@ import (
 	"github.com/worldiety/nago-demo/pages/interaction"
 	"github.com/worldiety/nago-demo/pages/typography"
 	"github.com/worldiety/nago-demo/util"
-
 	"github.com/worldiety/option"
 	"go.wdy.de/nago/application"
 	cfginspector "go.wdy.de/nago/application/inspector/cfg"
@@ -19,6 +20,7 @@ import (
 	flowbiteOutline "go.wdy.de/nago/presentation/icons/flowbite/outline"
 	heroOutline "go.wdy.de/nago/presentation/icons/hero/outline"
 	"go.wdy.de/nago/presentation/ui"
+	"go.wdy.de/nago/presentation/ui/alert"
 	"go.wdy.de/nago/web/vuejs"
 )
 
@@ -28,7 +30,7 @@ var appIcon application.StaticBytes
 func create() *application.Application {
 	return application.Configure(func(cfg *application.Configurator) {
 		cfg.SetApplicationID("de.worldiety.nago_test")
-		cfg.SetName("NAGO Test")
+		cfg.SetName("NAGO-Tester")
 		cfg.Serve(vuejs.Dist())
 
 		option.MustZero(cfg.StandardSystems())
@@ -41,15 +43,16 @@ func create() *application.Application {
 			cfg.NewScaffold().
 				Login(false).
 				Logo(ui.Image().Embed(appIcon).Frame(ui.Frame{Height: ui.L48})).
-				Footer(ui.VStack(
-					ui.Text("NAGO-Tester: "+util.Version()),
-					ui.Text("NAGO: "+util.DependencyVersion("go.wdy.de/nago")),
-				).Alignment(ui.Center).FullWidth(),
+				Footer(ui.HStack(
+					ui.Image().Embed(appIcon).Frame(ui.Frame{Height: ui.L48}),
+					versionsView(),
+				).Gap(ui.L32).Alignment(ui.Leading).FullWidth().Padding(ui.Padding{}.All(ui.L32)).Border(ui.Border{TopWidth: ui.L1, TopColor: ui.M5}),
 				).
 				MenuEntry().Title("Eingabe").Icon(flowbiteOutline.Keyboard).Forward("input").OneOfRole().
 				MenuEntry().Title("Inhalt").Icon(flowbiteOutline.Image).Forward("content").OneOfRole().
 				MenuEntry().Title("Interaktion").Icon(heroOutline.CursorArrowRays).Forward("interaction").OneOfRole().
-				MenuEntry().Title("Typografie").Icon(flowbiteOutline.TextSize).Forward("typography").OneOfRole().Decorator(),
+				MenuEntry().Title("Typografie").Icon(flowbiteOutline.TextSize).Forward("typography").OneOfRole().
+				MenuEntry().Dynamic(darkModeToggleMenuEntry()).Decorator(),
 		)
 
 		cfg.RootViewWithDecoration(".", func(wnd core.Window) core.View {
@@ -69,6 +72,69 @@ func create() *application.Application {
 			return typography.Page(wnd)
 		})
 	})
+}
+
+func darkModeToggleMenuEntry() func(wnd core.Window, entry *application.MenuEntryBuilder) {
+	return func(wnd core.Window, entry *application.MenuEntryBuilder) {
+		if wnd.Info().ColorScheme == core.Light {
+			entry.Title("Dark Mode")
+			entry.Icon(flowbiteOutline.Moon)
+			entry.Action(func(wnd core.Window) {
+				wnd.SetColorScheme(core.Dark)
+			})
+		} else {
+			entry.Title("Light Mode")
+			entry.Icon(flowbiteOutline.Sun)
+			entry.Action(func(wnd core.Window) {
+				wnd.SetColorScheme(core.Light)
+			})
+		}
+	}
+}
+
+func versionsView() core.View {
+	versionText := fmt.Sprintf("NAGO-Tester: %s\nNAGO: %s", util.Version(), util.DependencyVersion("go.wdy.de/nago"))
+
+	return ui.VStack(
+		ui.HStack(
+			ui.Text("Versionen:").
+				Font(ui.LabelSmall).
+				Color(ui.ColorText.WithTransparency(50)).
+				Padding(ui.Padding{Left: ui.L4}),
+		).Position(ui.Position{Type: ui.PositionAbsolute, Left: ui.L0, Bottom: ui.Full}),
+		ui.Text(versionText).Font(ui.MonoMedium),
+	).
+		NoClip(true).
+		Position(ui.Position{Type: ui.PositionRelative}).
+		Responsive(func(wnd core.Window, stack ui.TStack) ui.TStack {
+			copyVersionsState := core.StateOf[bool](wnd, "copyVersionsState")
+
+			stack = stack.Append(
+				ui.If(copyVersionsState.Get(),
+					ui.HStack(
+						ui.Text("Kopiert!").
+							Font(ui.LabelSmall).
+							Color(ui.ColorText.WithTransparency(50)).Padding(ui.Padding{Left: ui.L4}),
+					).Position(ui.Position{Type: ui.PositionAbsolute, Right: ui.L0, Bottom: ui.Full}),
+				),
+			)
+
+			return stack.Action(func() {
+				if err := wnd.Clipboard().SetText(versionText); err != nil {
+					alert.ShowBannerError(wnd, err)
+					copyVersionsState.Set(false)
+					return
+				}
+
+				if !copyVersionsState.Get() {
+					copyVersionsState.Set(true)
+					go func() {
+						time.Sleep(2 * time.Second)
+						copyVersionsState.Set(false)
+					}()
+				}
+			})
+		}).HoveredBackgroundColor(ui.ColorText.WithTransparency(85)).Padding(ui.Padding{}.Vertical(ui.L2).Horizontal(ui.L4))
 }
 
 // the main function of the program, which is like the java public static void main.
